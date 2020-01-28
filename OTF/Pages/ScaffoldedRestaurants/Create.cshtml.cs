e751720ace653beb74229fb5f5ab3e01d15eb583
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-01-28 17:49:26 +0100

    scaffold pages

---
+++ OTF/Pages/ScaffoldedRestaurants/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OTF.Core;
+using OTF.Data;
+
+namespace OTF
+{
+    public class CreateModel : PageModel
+    {
+        private readonly OTF.Data.OTFDbContext _context;
+
+        public CreateModel(OTF.Data.OTFDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Restaurants.Add(Restaurant);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ OTF/Pages/ScaffoldedRestaurants/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OTF.Core;
+using OTF.Data;
+
+namespace OTF
+{
+    public class IndexModel : PageModel
+    {
+        private readonly OTF.Data.OTFDbContext _context;
+
+        public IndexModel(OTF.Data.OTFDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Restaurant> Restaurant { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Restaurant = await _context.Restaurants.ToListAsync();
+        }
+    }
+}
